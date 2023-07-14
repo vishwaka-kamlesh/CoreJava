ff5771808d57069dfa428c7a4c066666c253e31a
vishwaka-kamlesh <137268987+vishwaka-kamlesh@users.noreply.github.com>
2023-07-14 13:35:50 +0700

    Create PrimitiveDataTypes.java

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,40 @@
+// WRITE AJAVA PROGRAM TO DEMO ABOUT ALL JAVA PRIMITIVE DATATYPES?
+
+class PrimitiveDataTypes {
+    
+    public static void main(String[] args) {
+
+        // boolean
+        boolean bool = true;
+        System.out.println("boolean: " + bool);
+
+        // byte
+        byte byteVar = 127;
+        System.out.println("byte: " + byteVar);
+
+        // short
+        short shortVar = 32767;
+        System.out.println("short: " + shortVar);
+
+        // int
+        int intVar = 2147483647;
+        System.out.println("int: " + intVar);
+
+        // long
+        long longVar = 9223372036854775807L;
+        System.out.println("long: " + longVar);
+
+        // float
+        float floatVar = 3.14159f;
+        System.out.println("float: " + floatVar);
+
+        // double
+        double doubleVar = 3.141592653589793;
+        System.out.println("double: " + doubleVar);
+
+        // char
+        char charVar = 'A';
+        System.out.println("char: " + charVar);
+
+    }
+}
