232fb61aad4b7b2ad973b2e029fbf34067f07631
vishwaka-kamlesh <137268987+vishwaka-kamlesh@users.noreply.github.com>
2023-07-14 13:32:40 +0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// WRITE JAVA PROGRAM TO PRINT "HELLO WORLD" MESSAGE?
+
+class HelloWorld {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    
+    }
+}
